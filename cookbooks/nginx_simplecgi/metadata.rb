c6cacb59dfaaf0012e65c25171c15bb8362b0f24
Appricot144 <aniline.96.256@gmail.com>
2022-06-09 13:38:59 +0900

    Import nginx_simplecgi version 0.3.4

---
+++ cookbooks/nginx_simplecgi/metadata.rb
@@ -0,0 +1,18 @@
+name             'nginx_simplecgi'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
+license          'Apache-2.0'
+description      'Provides SimpleCGI for NGINX'
+version          '0.3.4'
+
+%w( debian ubuntu redhat centos fedora scientific amazon oracle ).each do |os|
+  supports os
+end
+
+%w( nginx perl runit yum-epel ).each do |dep|
+  depends dep
+end
+
+source_url 'https://github.com/sous-chefs/nginx_simplecgi'
+issues_url 'https://github.com/sous-chefs/nginx_simplecgi/issues'
+chef_version '>= 12.19'
